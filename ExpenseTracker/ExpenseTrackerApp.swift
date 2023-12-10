1d5f5adc854db469bfb0782808d18b76af8ac301
Louis Farmer <louisfarmer@live.co.uk>
2023-12-10 15:47:57 -0800

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Louis Farmer on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
